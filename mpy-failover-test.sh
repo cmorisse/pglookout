300318c5025b53d682811145ed246f29fefe5656
Cyril MORISSE <cmorisse@boxes3.net>
2020-04-23 08:31:26 +0000

    IMP: Add mpy-failover script

---
+++ mpy-failover-test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#curl --header 'muppy-static-token: blabla' https://dev.cyril-dev.muppy.ovh/mpy/api/v1/pg_failover?cluster_name=v11-main-5432@mpypg-gr-1.muppy.ovh&no_test=1
+curl --header 'muppy-static-token: blabla' https://dev.cyril-dev.muppy.ovh/mpy/api/v1/pg_failover?cluster_name=v11-main-5432@mpypg-gr-1.muppy.ovh
