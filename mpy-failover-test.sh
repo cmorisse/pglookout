5aa7fa4c536495e79355e7cf1e0f29b30985c184
Cyril MORISSE <cmorisse@boxes3.net>
2020-04-23 15:26:46 +0000

    FIX: failover script missing "

--- mpy-failover-test.sh
+++ mpy-failover-test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-#curl --header 'muppy-static-token: blabla' https://dev.cyril-dev.muppy.ovh/mpy/api/v1/pg_failover?cluster_name=v11-main-5432@${HOSTNAME}.muppy.ovh&no_test=1
-curl --header 'muppy-static-token: blabla' https://dev.cyril-dev.muppy.ovh/mpy/api/v1/pg_failover?cluster_name=v11-main-5432@${HOSTNAME}.muppy.ovh
+#curl --header 'muppy-static-token: blabla' "https://dev.cyril-dev.muppy.ovh/mpy/api/v1/pg_failover?cluster_name=v11-main-5432@${HOSTNAME}.muppy.ovh&no_test=1"
+#curl --header 'muppy-static-token: blabla' "https://dev.cyril-dev.muppy.ovh/mpy/api/v1/pg_failover?cluster_name=v11-main-5432@${HOSTNAME}.muppy.ovh"
+curl --header 'muppy-static-token: blabla' "https://dev.cyril-dev.muppy.ovh/mpy/api/v1/pg_failover?cluster_name=v11-main-5432@${HOSTNAME}.muppy.ovh&no_test=1"
