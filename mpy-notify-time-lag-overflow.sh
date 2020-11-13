568b8b577575b5ba626c4220c8ba885ffc52ac79
Cyril MORISSE <cmorisse@boxes3.net>
2020-11-13 08:45:01 +0100

    WIO: Adjust scripts for new dev env

--- mpy-notify-time-lag-overflow.sh
+++ mpy-notify-time-lag-overflow.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl --header 'muppy-static-token: blabla' "https://dev.cyril-dev.muppy.ovh/mpy/api/v1/pgha_notify_event?cluster_name=v11-main-5432@${HOSTNAME}.muppy.ovh&event=OVER_WARNING_REPLICATION_TIME_LAG"
+curl --header 'muppy-static-token: blabla' "https://muppy-dev-cyril.odizy.ovh/mpy/api/v1/pgha_notify_event?cluster_name=v11-main-5432@$(hostname).muppy.ovh&event=OVER_WARNING_REPLICATION_TIME_LAG"
--- mpy-notify.sh
+++ mpy-notify.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl --header 'muppy-static-token: blabla' "https://dev.cyril-dev.muppy.ovh/mpy/api/v1/pgha_notify_event?cluster_name=$1&event=$2"
+curl --header 'muppy-static-token: blabla' "https://muppy-dev-cyril.odizy.ovh/mpy/api/v1/pgha_notify_event?cluster_name=$1&event=$2"
